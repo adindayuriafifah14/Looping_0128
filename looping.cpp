f03b80f173915400525f4cf6dbcaf4eeeecc3ecc
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-03-31 19:36:18 +0800

    Menambahkan perulangan for

--- looping.cpp
+++ looping.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include <time>
+#include <ctime>
 using namespace std;
 
 int main(){
     int x;
+
+    //Perulangan dengan FOR
+    //Mencetak tulisan sebanyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+    //untuk mendapatkan urutan angka acak yang berbeda
+    srand(time(0));
+
     
+
 }
